3f2ec4be3ed7b768265522af8ec15a1d6972897b
adam.mach <adam.mach@VT1-15>
2023-06-12 10:57:49 +0200

    Přidejte soubory projektu.

---
+++ LinkaMetra/Metralinka.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkaMetra
+{
+    internal class Metralinka
+    {
+        public string NázevLinky { get; set; }
+        public bool aktivni { get; set; }
+        public string[] stanice { get; set; }
+
+    }
+}
---
+++ LinkaMetra/Program.cs
@@ -0,0 +1,11 @@
+﻿using LinkaMetra ;
+Metralinka linkametra = new Metralinka();
+
+string[] poleLinkaA = {"Nemocnice Motol","Petřiny","Nádraží Veleslavín","Bořislavka","Dejvicka","Hradčanská","Malostranská","Staroměstká","Můstek","Muzeum","Náměstí Míru"
+, "Jiřího z Poděbrad","FLORA","ŽELIVSKÉHO","SKALKA","DEPO HOSTIVAŘ"};
+
+string[] poleLinkaB = {"Zličín","STODŮLKY","LUKA","LUŽINY","¨HŮRKA","NOVÉ BUTOVICE","JINONICE","RADLICKÁ","SMÍCHOVKSÉ NÁDRAŽÍ","ANDĚL","KARLOVO NÁMĚSTÍ","národní třída",
+"MŮSTEK","NÁMĚSTÍ REPUBLIKY","FLORENC","KŘÍŽKOVA","INVALIDOVNA","PALMOVKA","ČESKOMORAVSKÁ","VYSOČANSKÁ","KOLBENOVA","HLOUBĚTÍN","RAJSKÁ ZAHRADA","ČERNÝ MOST"};
+
+string[] poleLinkaC = { "Letnany", "Prosek", "Střížkov", "Ládví", "Kobylisy", "Nádraží Holešovice", "Vltavská", "Florenc", "Hlavní Nádrazí", "Muzeum", "I.P Pavlova", "Vyšehrad", "Pražského Povstaní", "Pankrác", "Budějovická", "Kačerov", "Roztyly", "chodov", "Opatov ", "háje ", };
+
