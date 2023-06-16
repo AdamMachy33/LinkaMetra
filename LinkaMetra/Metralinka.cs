30ec89c8102c00bed6b2b75e6e7312f41c279fab
adam.mach <adam.mach@VT1-15>
2023-06-16 12:41:35 +0200

    dokonceni

--- LinkaMetra/Metralinka.cs
+++ LinkaMetra/Metralinka.cs
@@ -11,6 +11,62 @@ namespace LinkaMetra
         public string NázevLinky { get; set; }
         public bool aktivni { get; set; }
         public string[] stanice { get; set; }
+        public string Barva { get; set; }   
+        public void VypisStanice()
+        {
+            if (stanice.Length > 0)
+            {
+                foreach (var jednastan in stanice) 
+                {
+                    Console.WriteLine(jednastan);
+                }
+            }
+        }
+        public void VypisPrvniPosledni()
+        {
+            if (stanice.Length >= 2)
+            {
+                Console.WriteLine(stanice[0]);
+                Console.WriteLine(stanice[stanice.Length-1]);
+            }
+        }
+        public void nastavlinku()
+        {
+            if(Barva =="zelená")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (Barva == "červená")
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+            }
+            else if (Barva == "žlutá")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (Barva == "modrá")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            Console.WriteLine($"vítej v lince metra {NázevLinky}");
+
+        }
+        public void CasCesty()
+        {
+            VypisStanice();
+            Console.WriteLine("vyber první stanici ");
+            string prvniStanice = Console.ReadLine();
+            int indexPrvni = Array.IndexOf(stanice, prvniStanice);
+            Console.WriteLine("-------");
+
+            Console.WriteLine("vyberou druhou stanici ");
+            string druhastanice = Console.ReadLine();
+            int indexdruhe = Array.IndexOf(stanice, druhastanice);
+            Console.WriteLine("--------");
+            int dobacesty = (indexPrvni + indexdruhe)*2;
+            Console.WriteLine("doba cesty "+ dobacesty);
+        }
+
 
     }
 }
--- LinkaMetra/Program.cs
+++ LinkaMetra/Program.cs
@@ -9,3 +9,66 @@ string[] poleLinkaB = {"Zličín","STODŮLKY","LUKA","LUŽINY","¨HŮRKA","NOVÉ
 
 string[] poleLinkaC = { "Letnany", "Prosek", "Střížkov", "Ládví", "Kobylisy", "Nádraží Holešovice", "Vltavská", "Florenc", "Hlavní Nádrazí", "Muzeum", "I.P Pavlova", "Vyšehrad", "Pražského Povstaní", "Pankrác", "Budějovická", "Kačerov", "Roztyly", "chodov", "Opatov ", "háje ", };
 
+Console.WriteLine("zadej linku kterou chčeš využít ");
+string volbalinky = Console.ReadLine();
+if (volbalinky == "A")
+{
+    Metralinka LinkaA = new Metralinka() { NázevLinky = "A", Barva = "zelená", aktivni = true, stanice = poleLinkaA };
+    LinkaA.nastavlinku();
+    Console.WriteLine("chcete vypsat 1. všechny stanice 2. první a poslední 3. zjistit čas cesty");
+    string volba = Console.ReadLine();
+    if (volba =="1")
+    {
+        LinkaA.VypisStanice();
+    }
+    else if (volba =="2")
+    {
+        LinkaA.VypisPrvniPosledni();
+    }
+    else if (volba == "3")
+    {
+        LinkaA.CasCesty();
+    }
+
+}
+if (volbalinky == "B")
+{
+    Metralinka LinkaB = new Metralinka() { NázevLinky = "B", Barva = "červená", aktivni = true, stanice = poleLinkaB };
+    LinkaB.nastavlinku();
+    Console.WriteLine("chcete vypsat 1. všechny stanice 2. první a poslední  3. zjistit čas cesty ");
+    string volba = Console.ReadLine();
+    if (volba == "1")
+    {
+        LinkaB.VypisStanice();
+    }
+    else if (volba == "2")
+    {
+        LinkaB.VypisPrvniPosledni();
+    }
+    else if (volba == "3")
+    {
+        LinkaB.CasCesty();
+    }
+
+}
+if (volbalinky == "C")
+{
+    Metralinka LinkaC = new Metralinka() { NázevLinky = "C", Barva = "žlutá", aktivni = true, stanice = poleLinkaC };
+    LinkaC.nastavlinku();
+    Console.WriteLine("chcete vypsat 1. všechny stanice 2. první a poslední  3. zjistit čas cesty ");
+    string volba = Console.ReadLine();
+    if (volba == "1")
+    {
+        LinkaC.VypisStanice();
+    }
+    else if (volba == "2")
+    {
+        LinkaC.VypisPrvniPosledni();
+    }
+    else if (volba == "3")
+    {
+        LinkaC.CasCesty();
+    }
+
+}
+
